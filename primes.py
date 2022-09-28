e56fcd93b93cca15299c440756194f309d11c79f
Ivo Verneris <ivo.verneris@kcl.ac.uk>
2022-09-29 00:16:34 +0100

    Add functionality to primes function in primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes < 1:
+        raise ValueError("Only numbers greaater than 0")
+    else:
+        list = []
+        primeCount = 0
+        loopCount = 1
+        while primeCount < number_of_primes:
+            loopCount += 1
+            for y in range(2, loopCount):
+                if loopCount % y == 0:
+                    break
+            else:
+                list.append(loopCount)
+                primeCount += 1
+        return list
